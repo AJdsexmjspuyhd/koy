6ba082e7dc4f185c06218fb4c16939bf05072c7b
AJdsexmjspuyhd <154853936+AJdsexmjspuyhd@users.noreply.github.com>
2023-12-29 01:29:50 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const { TextDecoder } = require('util');
 const logcb= (...args)=>console.log.bind(this,...args);
 const errcb= (...args)=>console.error.bind(this,...args);
 
-const uuid= (process.env.UUID||'d342d11e-d424-4583-b36e-524ab1f0afa4').replace(/-/g, "");
+const uuid= (process.env.UUID||'820a9261-260f-4466-9a31-171a74f47d68').replace(/-/g, "");
 const port= process.env.PORT||3000;
 
 const wss=new WebSocket.Server({port},logcb('listen:', port));
